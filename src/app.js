e565d315a9ab68d013cb24d2fc2b8a2a877f118c
Courtney de Lautour <courtney.de.lautour@gmail.com>
2016-07-06 23:37:22 +0800

    Tests for the high level #run function

--- src/app.js
+++ src/app.js
@@ -1,7 +1,29 @@
 const Rx = require('rx')
-exports.run = (input) => {
+
+function executeCommand(command){
+  if(command.match(/report/i))
     return {
-        "std$": Rx.Observable.empty(),
-        "error$": input.select(cmd => `The command ' ${cmd} ' could not be interpreted`)
+      std: '1,1 NORTH'
+    }
+  if(command.match(/place/i)){
+    return {}
+  }
+
+  return {
+    err: `The command ' ${command} ' could not be interpreted`
+  }
+}
+
+function selectPresentKeys(object$, key){
+  return object$.where(obj => key in obj).select(obj => obj[key])
+}
+
+
+exports.run = (input) => {
+    const commandResult$ = input.select(executeCommand).share()
+
+    return {
+        'std$': selectPresentKeys(commandResult$, 'std'),
+        'error$': selectPresentKeys(commandResult$, 'err')
     };
 }
--- test/app.js
+++ test/app.js
@@ -4,45 +4,70 @@ const app = require('../src/app')
 
 
 describe('app.js', () => {
-  let output = null;
+  describe('#run', () => {
+    let output = null;
 
-  beforeEach(() => {
-      output = Rx.ReplaySubject()
-  })
-
-  describe('when there is no input', () => {
-    const input$ = new Rx.Observable.empty()
-    it('there no std output', (done) => {
-      app.run(input$).std$.count().subscribe(count => {
-        expect(count).to.eql(0)
-        done()
-      })
+    beforeEach(() => {
+        output = Rx.ReplaySubject()
     })
-    it('there no error output', (done) => {
-      app.run(input$).error$.count().subscribe(count => {
-        expect(count).to.eql(0)
-        done()
+
+    describe('when there is no input', () => {
+      const input$ = new Rx.Observable.empty()
+      it('there no std output', (done) => {
+        app.run(input$).std$.count().subscribe(count => {
+          expect(count).to.eql(0)
+          done()
+        })
       })
-    })
-  })
-
-  describe('when there is jibberish input', () => {
-    const jibberish = 'asoeuhsaoteu'
-
-    const input$ = new Rx.Observable.just(jibberish)
-
-    it('there no std output', (done) => {
-      app.run(input$).std$.count().subscribe(count => {
-        expect(count).to.eql(0)
-        done()
+      it('there no error output', (done) => {
+        app.run(input$).error$.count().subscribe(count => {
+          expect(count).to.eql(0)
+          done()
+        })
       })
     })
 
-    it('there error explaining the error', (done) => {
-      app.run(input$).error$.first().subscribe(error => {
-        expect(error).to.eql(`The command ' ${jibberish} ' could not be interpreted`)
-        done()
+    describe('when there is jibberish input', () => {
+      const jibberish = 'asoeuhsaoteu'
+
+      const input$ = new Rx.Observable.just(jibberish)
+
+      it('there no std output', (done) => {
+        app.run(input$).std$.count().subscribe(count => {
+          expect(count).to.eql(0)
+          done()
+        })
       })
+
+      it('there error explaining the error', (done) => {
+        app.run(input$).error$.first().subscribe(error => {
+          expect(error).to.eql(`The command ' ${jibberish} ' could not be interpreted`)
+          done()
+        })
+      })
+    })
+
+    describe('when there is a REPORT command following a valid PLACE command', () => {
+
+      const place = 'PLACE 1,1 NORTH'
+      const report = 'REPORT'
+
+      const input$ = new Rx.Observable.from([place, report])
+
+      it('the status of the robot is reported', (done) => {
+        app.run(input$).std$.first().subscribe(output => {
+          expect(output).to.eql('1,1 NORTH')
+          done()
+        })
+      })
+
+      it('there no error output', (done) => {
+        app.run(input$).error$.count().subscribe(count => {
+          expect(count).to.eql(0)
+          done()
+        })
+      })
+
     })
   })
 })
