4d85ae94c56eb5f7cebd0c02f2d24134eee25474
Courtney de Lautour <courtney.de.lautour@gmail.com>
2016-08-08 21:38:49 +0800

    Add some better comments yet

--- src/app.js
+++ src/app.js
@@ -7,10 +7,10 @@ const directions = ['NORTH', 'EAST', 'SOUTH', 'WEST']
 
 /*
   The main loop
-  @param {Rx.Observable} input$ - A stream of commands to execute 
+  @param {Rx.Observable} input$ - A stream of commands to execute
 */
-function run(commands$) {
-  const commandResult$ = commands$
+function run(input$) {
+  const commandResult$ = input$
     .scan((state, command) => executeCommand(state.robotLocation, command), {})
     .share()
   return {
@@ -34,14 +34,14 @@ function executeCommand(robotLocation, command) {
     [/.*/, error]
   ]
 
-  const [argumentRe, handler] = _(commandList).find(([re,]) => command.match(re))
-  return handler(robotLocation, command.match(argumentRe))
+  const [regex, handler] = _(commandList).find(([regex,]) => command.match(regex))
+  return handler(robotLocation, ...command.match(regex))
 }
 
 /*
   Returns a rotated version of the robot location
   @param { number } turnDirection - a value representing the direction to turn. 1 for clockwise,  -1 for anti-clockwise
-  @param { Object } 
+  @param { Object } robotLocation - The current location of the robot
 */
 function turn(turnDirection, robotLocation) {
   if (!robotLocation)
@@ -54,6 +54,10 @@ function turn(turnDirection, robotLocation) {
 const turnLeft = _.curry(turn)(-1)
 const turnRight = _.curry(turn)(1)
 
+/*
+  Returns a moved version of the robot location
+  @param { Object } robotLocation - The current location of the robot
+*/
 function move(robotLocation) {
   if (!robotLocation)
     return { err: 'Robot has not been placed' }
@@ -64,9 +68,9 @@ function move(robotLocation) {
   Ascii art compass for help understanding the axis and movementDirection calcs
           N: 0
      W:3  -|-  E:1
-          S: 2       
+          S: 2
   */
-  const axis = currentDirectionIndex % 2 === 0 ? 'y' : 'x'; // North/South are even - East/West are odd 
+  const axis = currentDirectionIndex % 2 === 0 ? 'y' : 'x'; // North/South are even - East/West are odd
   const movementDirection = currentDirectionIndex < 2 ? 1 : -1 // Top/Right (0 and 1) are positive.
   const [min, max] = tableBounds[axis]
 
@@ -74,6 +78,10 @@ function move(robotLocation) {
   return { robotLocation: Object.assign({}, robotLocation, { [axis]: Math.max(min, Math.min(max, robotLocation[axis] + movementDirection)) }) }
 }
 
+/*
+  Returns the current location of the robot and a stdout message.
+  @param { Object } robotLocation - The current location of the robot
+*/
 function report(robotLocation) {
   if (!robotLocation)
     return { err: 'Robot has not been placed' }
@@ -84,7 +92,15 @@ function report(robotLocation) {
   }
 }
 
-function place(robotLocation, [cmd, x, y, direction]) {
+/*
+  Returns a new version of the robot location based on arguments. If the location is invalid, the current location is returned
+  @param { Object } robotLocation - The current location of the robot
+  @param { string } cmd - the full place command
+  @param { string } x - the x position
+  @param { string } y - the y position
+  @param { string } direction - the robots direction
+*/
+function place(robotLocation, cmd, x, y, direction) {
   if (!_(directions).includes(direction.toUpperCase())) {
     return { robotLocation, err: `${direction} must be one of ${directions}` }
   }
@@ -102,6 +118,11 @@ function place(robotLocation, [cmd, x, y, direction]) {
   }
 }
 
+/*
+  Returns the current location of the robot and a stderr message.
+  @param { Object } robotLocation - The current location of the robot
+  @param { string } command - The command which resulted in the error
+*/
 function error(robotLocation, command) {
   return {
     robotLocation,
@@ -109,6 +130,11 @@ function error(robotLocation, command) {
   }
 }
 
+/*
+  Returns the a stream of objects where the given key is present
+  @param { Rx.Observable<Object> } object$ - The stream of objects
+  @param { string } Key - The key to check for
+*/
 function _selectPresentKeys(object$, key) {
   return object$.where(obj => key in obj).select(obj => obj[key])
 }
